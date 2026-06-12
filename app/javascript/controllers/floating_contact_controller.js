a24cbc88e1474f8dbf3198cef74ec39b3d1ff393
Md Mahadi Hasan Rifat <mahadihasanrifat86@gmail.com>
2026-06-12 22:37:05 +0600

    fix few design

---
+++ app/javascript/controllers/floating_contact_controller.js
@@ -0,0 +1,20 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static values = {
+    storageKey: { type: String, default: "removlo:floating-contact-dismissed" }
+  }
+
+  connect() {
+    if (this.dismissed) this.element.remove()
+  }
+
+  close() {
+    localStorage.setItem(this.storageKeyValue, "true")
+    this.element.remove()
+  }
+
+  get dismissed() {
+    return localStorage.getItem(this.storageKeyValue) === "true"
+  }
+}
