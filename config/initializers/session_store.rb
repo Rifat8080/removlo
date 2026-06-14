6e8f1d3d25927f321e6d82cfb74e3612b1b8fe35
Md Mahadi Hasan Rifat <mahadihasanrifat86@gmail.com>
2026-06-14 09:02:28 +0600

    session store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,4 @@
-Rails.application.config.session_store(
-  :cookie_store,
+Rails.application.config.session_store :cookie_store,
   key: "_removlo_session",
-  secure: Rails.env.production?,
-  httponly: true,
-  same_site: :lax,
-  expire_after: 12.hours
-)
+  secure: false,
+  same_site: :lax
