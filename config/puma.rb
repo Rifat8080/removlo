3b3b1800c2f6fd9dced7675ac5b03888e5c4f3c4
Md Mahadi Hasan Rifat <mahadihasanrifat86@gmail.com>
2026-06-14 08:01:00 +0600

    puma

--- config/puma.rb
+++ config/puma.rb
@@ -1,5 +1,3 @@
-# Puma configuration for Removlo production
-
 max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
 
@@ -7,7 +5,6 @@ threads min_threads_count, max_threads_count
 
 environment ENV.fetch("RAILS_ENV") { "production" }
 
-# Nginx will proxy to this local Puma port
 bind "tcp://127.0.0.1:3000"
 
 pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
