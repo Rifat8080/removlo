40c97218b3d64a7b6916e3cb11026dbc0150b3ef
Md Mahadi Hasan Rifat <mahadihasanrifat86@gmail.com>
2026-06-15 09:48:49 +0600

    assets

--- config/puma.rb
+++ config/puma.rb
@@ -1,16 +1,23 @@
-max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
-min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
+# Puma configuration. See https://puma.io/puma/Puma/DSL.html
 
+max_threads_count = ENV.fetch("RAILS_MAX_THREADS", 5)
+min_threads_count = ENV.fetch("RAILS_MIN_THREADS", max_threads_count)
 threads min_threads_count, max_threads_count
 
-environment ENV.fetch("RAILS_ENV") { "production" }
+environment ENV.fetch("RAILS_ENV", "development")
 
-bind "tcp://127.0.0.1:3000"
+if ENV.fetch("RAILS_ENV", "development") == "production"
+  bind ENV.fetch("BIND", "tcp://127.0.0.1:#{ENV.fetch('PORT', 3000)}")
+else
+  port ENV.fetch("PORT", 3000)
+end
 
-pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
-
-workers ENV.fetch("WEB_CONCURRENCY") { 1 }
-
-preload_app!
+pidfile ENV["PIDFILE"] if ENV["PIDFILE"]
 
 plugin :tmp_restart
+
+workers_count = ENV.fetch("WEB_CONCURRENCY", "0").to_i
+if workers_count.positive?
+  workers workers_count
+  preload_app!
+end
